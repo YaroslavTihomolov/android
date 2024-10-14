bcca241d112994e4df73f8254e6528b4fcb5a700
YaroslavTihomolov <93517226+YaroslavTihomolov@users.noreply.github.com>
2024-10-14 18:54:20 +0700

    lab 2

---
+++ app/src/main/java/nsu/fit/tikhomolov/lab2/MainActivity.kt
@@ -0,0 +1,47 @@
+package nsu.fit.tikhomolov.lab2
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.recyclerview.widget.LinearLayoutManager
+import nsu.fit.tikhomolov.lab2.databinding.ActivityMainBinding
+import nsu.fit.tikhomolov.lab2.ui.theme.Lab2Theme
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: SongAdapter
+    private val songService: SongService
+        get() = (applicationContext as App).songService
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // If using Jetpack Compose
+        setContent {
+            Lab2Theme {
+                Surface {
+                    SongListScreen()
+                }
+            }
+        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val manager = LinearLayoutManager(this)
+        adapter = SongAdapter()
+        adapter.data = songService.getSongs()
+
+        binding.recyclerView.layoutManager = manager
+        binding.recyclerView.adapter = adapter
+    }
+}
+
+@Composable
+fun SongListScreen() {
+    // Here you can define how your song list looks using Jetpack Compose
+    Text("Song List") // Placeholder for your song list
+}
