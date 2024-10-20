89183192d50004c588294bb2700e4d923129ea93
YaroslavTihomolov <93517226+YaroslavTihomolov@users.noreply.github.com>
2024-10-20 20:21:07 +0700

    убрал автоген

--- lab2/app/src/main/java/nsu/fit/tikhomolov/lab2/MainActivity.kt
+++ lab2/app/src/main/java/nsu/fit/tikhomolov/lab2/MainActivity.kt
@@ -1,14 +1,11 @@
 package nsu.fit.tikhomolov.lab2
 
 import android.os.Bundle
-import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.recyclerview.widget.LinearLayoutManager
 import nsu.fit.tikhomolov.lab2.databinding.ActivityMainBinding
-import nsu.fit.tikhomolov.lab2.ui.theme.Lab2Theme
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -18,14 +15,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContent {
-            Lab2Theme {
-                Surface {
-                    SongListScreen()
-                }
-            }
-        }
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
